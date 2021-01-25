01f13627e0e573addbfb94809821c46e351c9a20
joey <jz.liu@outlook.com>
2021-01-25 18:37:40 +0800

    fix mysql conf

--- root/defaults/mysql/add_user.sql
+++ root/defaults/mysql/add_user.sql
@@ -1,4 +1,3 @@
-DELETE FROM mysql.user WHERE user='root' AND host='localhost';
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'nexusphprocks';
 GRANT ALL PRIVILEGES ON *.* to 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
